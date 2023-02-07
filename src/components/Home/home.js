d246ac600e69fc8356742a31d8a4f91c612d4804
DspSeven <119715565+DspSeven@users.noreply.github.com>
2023-02-07 09:44:09 +0530

    Update home.js

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,22 +1,23 @@
 // import Cookies from 'js-cookie'
 import {Link} from 'react-router-dom'
 import Header from '../Header/header'
-
-// import './home.css'
+import './home.css'
 
 const Home = () => {
   console.log('home')
   return (
-    <div>
+    <div className="home-container">
       <Header />
-      <div>
-        <h1>Find The Job That Fits Your Life</h1>
-        <p>
-          Millions of people are searching for jobs, salary information, company
-          reviews. Find the jobs that fits your abilities and potential
+      <div className="bg-container">
+        <h1 className="home-heading">Find The Job That Fits Your Life</h1>
+        <p className="para-heading">
+          Millions of people are searching for jobs, salary information, <br />
+          company reviews. Find the jobs that fits your abilities and potential
         </p>
         <Link to="/jobs">
-          <button type="button">Find Jobs</button>
+          <button type="button" className="home-button">
+            Find Jobs
+          </button>
         </Link>
       </div>
     </div>
